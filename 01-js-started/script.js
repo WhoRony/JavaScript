cdb81c2465f2d94bd52cf4fd73fb281316ba6808
Rohan <ronymalik.2604@gmail.com>
2026-01-16 23:26:13 +0530

    added md file

--- 01-js-started/script.js
+++ 01-js-started/script.js
@@ -1,2 +1,34 @@
-//Write a function sayHello() that prints "hello js"
+//Write a function sayHello() that prints "hello js".
+// function sayHello(){
+//     console.log("hello js");
+// }
+// sayHello()
 //create a function add(a, b) that returns their sum and log the result
+// function add(a,b){
+//     return a+b
+// }
+// abc = add(1,2)
+// console.log(abc);
+
+//Write a function wiht a default parameter name = "Guest" and prints "hi <name>"
+// function(name="Guest"){
+//     return name
+// }
+
+// abc= text()
+
+//use rest parameters to make a funciton that adds unlimited numbers.
+
+
+
+// Create an IIFE that prints "i run instantly"
+
+// (function abc(name){
+//     console.log("i run instantly");
+    
+// })()
+//Make a nested funciton wherer the inner one prints a variable from the outer one
+// create an array of 5 fruits. Add one at the end and remove one from the beginning 
+// use a for loop to print all elements of an array
+// create an object person with key name,age and city and print each key's value
+// Use setTimeout() to log "Time's up!" after 2 seconds 
