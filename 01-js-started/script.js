5119df06a35efb2b12483e1682f5f909f3c20e54
Rohan <ronymalik.2604@gmail.com>
2026-01-17 22:04:50 +0530

    added questions

--- 01-js-started/script.js
+++ 01-js-started/script.js
@@ -19,16 +19,14 @@
 
 //use rest parameters to make a funciton that adds unlimited numbers.
 
-
-
 // Create an IIFE that prints "i run instantly"
 
 // (function abc(name){
 //     console.log("i run instantly");
-    
+
 // })()
 //Make a nested funciton wherer the inner one prints a variable from the outer one
-// create an array of 5 fruits. Add one at the end and remove one from the beginning 
+// create an array of 5 fruits. Add one at the end and remove one from the beginning
 // use a for loop to print all elements of an array
 // create an object person with key name,age and city and print each key's value
-// Use setTimeout() to log "Time's up!" after 2 seconds 
+// Use setTimeout() to log "Time's up!" after 2 seconds
