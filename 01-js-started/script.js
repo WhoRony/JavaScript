a90b55219e35e23285647c5d5697e671f3070181
Rohan <ronymalik.2604@gmail.com>
2026-01-29 22:10:57 +0530

    solved some problems

--- 01-js-started/script.js
+++ 01-js-started/script.js
@@ -1,32 +1,95 @@
-//Write a function sayHello() that prints "hello js".
-// function sayHello(){
-//     console.log("hello js");
-// }
-// sayHello()
-//create a function add(a, b) that returns their sum and log the result
-// function add(a,b){
-//     return a+b
-// }
-// abc = add(1,2)
-// console.log(abc);
+//function for last element of the array
+// let arr = [1,2,3,4,5]
 
-//Write a function wiht a default parameter name = "Guest" and prints "hi <name>"
-// function(name="Guest"){
-//     return name
+// let arr2 = [2,34,3,423,4,235,23,52,352,34,2,42,432,42,42,424,4]
+
+// function lastElement(myarr){
+//     return myarr[myarr.length-1]
 // }
 
-// abc= text()
+// console.log(lastElement(arr));
 
-//use rest parameters to make a funciton that adds unlimited numbers.
+//Combination of 2 arrays
+// let arr = [1,2,3,4,5]
+// let arr2 = [6,7,8,9,0]
+// console.log(...arr ,...arr2)
 
-// Create an IIFE that prints "i run instantly"
 
-// (function abc(name){
-//     console.log("i run instantly");
+// function randomInt(min,max){
+//     return Math.floor(Math.random()* (max - min +1) + min)
+// }
 
-// })()
-//Make a nested funciton wherer the inner one prints a variable from the outer one
-// create an array of 5 fruits. Add one at the end and remove one from the beginning
-// use a for loop to print all elements of an array
-// create an object person with key name,age and city and print each key's value
-// Use setTimeout() to log "Time's up!" after 2 seconds
+// console.log(randomInt(1,18));
+
+
+// let arr = [1,2,3,"sd",4,"a","as","dasd",5]
+
+// let newArray = arr.filter((n)=>{
+//     return typeof n == 'string'
+// })
+
+// console.log(newArray);
+
+
+// let arr = [1,2,312,41,412,41,21,23,124,123,123,12312]
+
+
+// let exarr = arr.sort((a,b)=>a-b)
+
+// console.log(exarr[exarr.length-1]);
+
+
+// let obj = {
+//     name:"rasd",
+//     type: "sdad",
+//     number:12123
+// }
+
+// function lenObj(object){
+//     return Object.keys(object).length;
+// }
+
+// let ans = lenObj(obj)
+// console.log(ans);
+
+// const people = [
+//   { name: "John", gender: "male" },
+//   { name: "Jane", gender: "female" },
+//   { name: "Julie", gender: "male" },
+//   { name: "Alice", gender: "female" },
+//   { name: "Bob", gender: "male" },
+//   { name: "Charlie", gender: "non-binary" }
+// ];
+
+// let newArray = people.filter((n)=> n.gender == "male")
+// console.log(newArray);
+
+
+// let arr = ["a","a","a","a","a","a","a"]
+
+// let nwearr = arr.map(element => {
+//     return element.toUpperCase()
+// });
+
+// console.log(nwearr);
+
+// let obj = {}
+
+
+// function nokey(obj){
+//     if ( Object.keys(obj).length === 0){
+//         return "Nothing"
+//     }
+//     return "Has data"
+// }
+
+// console.log(nokey(obj));
+
+// let arr = [1,2,3,4,5,6]
+
+// let newarr =  arr.map((e)=>{
+//     return e*e
+// })
+
+// console.log(newarr);
+
